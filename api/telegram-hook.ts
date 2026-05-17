990bad03399095b1f260de42d5c4cda43f43e404
zenayorcloud <zenayorcloud@gmail.com>
2026-05-17 20:28:05 +0000

    Update telegram-hook.ts

--- api/telegram-hook.ts
+++ api/telegram-hook.ts
@@ -39,7 +39,7 @@ export async function handleStartCommand(ctx) {
     console.log(`Reply to ${COMMAND} command sent successfully.`);
 
     // Wait 5 seconds BEFORE the function returns
-    await sleep(5000);
+    await sleep(60 * 1000);
 
     // Delete user's /start message
     try {
