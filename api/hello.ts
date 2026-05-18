22fc432466e5d9aa1a252fc22a265f7763b423e8
zenayorcloud <zenayorcloud@gmail.com>
2026-05-18 00:21:26 +0000

    Delete api/hello.ts

--- api/hello.ts
+++
@@ -1,7 +0,0 @@
-import { VercelRequest, VercelResponse } from "@vercel/node"
-
-
-// We only keep this in for testing purposes.
-export default (req: VercelRequest, res: VercelResponse) => {
-  return res.send("Hello World")
-}
